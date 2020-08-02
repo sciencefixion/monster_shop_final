a6c6dd1e60c49d406a2985b12aee61d3266675e8
Rion Hutsell <st.gish@gmail.com>
2020-08-02 17:51:41 -0600

    Create specs for discount model and discounts index

---
+++ spec/features/discounts/index_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+
+RSpec.describe 'Bulk discount index page' do
+  before :each do
+    @lemarchand = Merchant.create(name: "LeMarchand Boxes", address: '1717 Rue de L\'Académie Royale', city: 'Paris', state: 'TX', zip: 75460)
+    @item1 = @lemarchand.items.create(name: "Lament Configuration", description: "We have such sights to show you!", price: 999, image: "https://vignette.wikia.nocookie.net/cenobite/images/f/fa/Lament_Configuration.jpg", inventory: 999 )
+    @m_user = @lemarchand.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
+    @discount_1 = @lemarchand.discounts.create(item_name: "#{@item1.name}", required_quantity: 20, percentage: .05)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
+  end
+
+  it "allows bulk discounts to be viewed from a link on the Merchant dashboard" do
+    visit '/merchant'
+
+    expect(page).to have_link('Bulk Discounts')
+
+    expect(current_path).to eq("/merchant/discounts")
+
+
+    expect(page).to have_content("#{@item1.name}")
+    expect(page).to have_content("Required quantity: 20")
+    expect(page).to have_content("Discount percentage: 5")
+
+  end
+end
---
+++ spec/models/discount_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Discount do
+  describe 'Relationships' do
+    it {should belong_to :merchant}
+
+
+  end
+
+  describe 'Validations' do
+    it {should validate_presence_of :item_name}
+    it {should validate_presence_of :required_quantity}
+    it {should validate_presence_of :percentage}
+  end
+end
