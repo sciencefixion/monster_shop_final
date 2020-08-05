617cba4d7fc6ab3997ce3ad2c3c15d5dd684aa79
Rion Hutsell <st.gish@gmail.com>
2020-08-04 20:46:43 -0600

    Remove validation of item name from discount model

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,7 +1,6 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
 
-  validates_presence_of :item_name,
-                        :required_quantity,
+  validates_presence_of :required_quantity,
                         :percentage
 end
