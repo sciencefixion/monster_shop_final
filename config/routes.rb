4a08df45622fa20c5a2d5c6e05e8d54cc54e68cc
Rion Hutsell <st.gish@gmail.com>
2020-08-15 13:28:51 -0600

    Rewrite routes merchant_items, merchants, items, reviews, users, profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,75 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Original routes are commented out above newly rewritten routes
+
+  # get :root, to: 'welcome#index'
   root 'welcome#index'
-  get :root, to: 'welcome#index'
 
-  resources :merchants do
-    resources :items, only: [:index]
-  end
+  # resources :merchants do
+  #   resources :items, only: [:index]
+  # end
+  get '/merchants/:merchant_id/items', to: 'items#index', as: 'merchant_items'
 
-  resources :items, only: [:index, :show] do
-    resources :reviews, only: [:new, :create]
-  end
+  get '/merchants', to: 'merchants#index'
+  post '/merchants', to: 'merchants#create'
+  get '/merchants/new', to: 'merchants#new', as: 'new_merchant'
+  get '/merchants/:id/edit', to: 'merchants#edit', as: 'edit_merchant'
+  get '/merchants/:id', to: 'merchants#show', as: 'merchant'
+  patch '/merchants/:id', to: 'merchants#update'
+  delete '/merchants/:id', to: 'merchants#destroy'
 
-  resources :reviews, only: [:edit, :update, :destroy]
+
+  # resources :items, only: [:index, :show] do
+  #   resources :reviews, only: [:new, :create]
+  # end
+  post '/items/:item_id/reviews', to: 'reviews#create', as: 'item_reviews'
+  get '/items/:item_id/reviews/new', to: 'reviews#new', as: 'new_item_review'
+
+  get '/items', to: 'items#index'
+  get '/items/:id', to: 'items#show', as: 'item'
+
+
+  # resources :reviews, only: [:edit, :update, :destroy]
+  get '/reviews/:id/edit', to: 'reviews#edit', as: 'edit_review'
+  patch '/reviews/:id', to: 'reviews#update', as: 'review'
+  put '/reviews/:id', to: 'reviews#update'
+  delete '/reviews/:id', to: 'reviews#destroy'
+
 
   get '/cart', to: 'cart#show'
+  # resources :cart, only: [:show]
+
   post '/cart/:item_id', to: 'cart#add_item'
   delete '/cart', to: 'cart#empty'
   patch '/cart/:change/:item_id', to: 'cart#update_quantity'
   delete '/cart/:item_id', to: 'cart#remove_item'
 
+
   get '/registration', to: 'users#new', as: :registration
-  resources :users, only: [:create, :update]
-  patch '/user/:id', to: 'users#update'
-  get '/profile', to: 'users#show'
-  get '/profile/edit', to: 'users#edit'
-  get '/profile/edit_password', to: 'users#edit_password'
+
+  # resources :users, only: [:create, :update]
+  post '/users', to: 'users#create', as: 'users'
+  patch '/users/:id', to: 'users#update', as: 'user'
+  put '/users/:id', to: 'users#update'
+  patch '/users/:id', to: 'users#update'
+
+
+  # get '/profile', to: 'users#show'
+  # get '/profile/edit', to: 'users#edit'
+  # get '/profile/edit_password', to: 'users#edit_password'
+  scope :profile do
+    get '/', to: 'users#show', as: 'profile'
+    get '/edit', to: 'users#edit', as: 'profile_edit'
+    get '/edit_password', to: 'users#edit_password', as: 'profile_edit_password'
+  end
+
+
   post '/orders', to: 'user/orders#create'
   get '/profile/orders', to: 'user/orders#index'
   get '/profile/orders/:id', to: 'user/orders#show'
   delete '/profile/orders/:id', to: 'user/orders#cancel'
+
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#login'
